d1be599e252c5a5b949b2a8cb6f326e8f7e508c1
Aaron Jonk <AJ@jonk.name>
2024-08-20 20:26:53 +0200

    Guess who couldn't make up his mind and just switchd to pancakeswap???

--- src/contracts/ChainPay.sol
+++ src/contracts/ChainPay.sol
@@ -9,19 +9,19 @@ import "./IWrapped.sol";
 import "./TransferHelper.sol";
 
 
-contract ChainPay is Ownable {
+abstract contract ChainPay is Ownable {
     ISwapRouter public immutable swapRouter;
     IWrapped public immutable wrappedCoin;
-    address public constant SUSHISWAP_V3_ROUTER_ADDRESS = 0x909662a99605382dB1E8d69cc1f182bb577d9038; // BNB chain
+    address public constant PANCAKESWAP_V3_ROUTER_ADDRESS = 0x1b81D678ffb9C0263b24A97847620C99d213eB14; // BNB chain
     address public constant WRAPPED_COIN = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c; // BNB chain, wrapped BNB
 
     constructor() {
-        swapRouter = ISwapRouter(SUSHISWAP_V3_ROUTER_ADDRESS);
+        swapRouter = ISwapRouter(PANCAKESWAP_V3_ROUTER_ADDRESS);
         wrappedCoin = IWrapped(WRAPPED_COIN);
     }
 
-    function swap(address tokenIn, address tokenOut, uint256 amountIn, uint256 amountOutMax, uint24 fee) internal returns (uint256 amountIn) {
-        TransferHelper.safeApprove(tokenIn, address(swapRouter), tokenIn);
+    function swap(address tokenIn, address tokenOut, uint256 amountIn, uint256 amountOutMax, uint24 fee) internal returns (uint256) {
+        TransferHelper.safeApprove(tokenIn, address(swapRouter), amountIn);
 
         ISwapRouter.ExactOutputSingleParams memory params = ISwapRouter.ExactOutputSingleParams({
             tokenIn: tokenIn,
@@ -29,7 +29,7 @@ contract ChainPay is Ownable {
             fee: fee, // 3000 = 0.3%
             recipient: address(this),
             deadline: block.timestamp,
-            amountOut: amountOut,
+            amountOut: amountOutMax,
             amountInMaximum: amountIn,
             sqrtPriceLimitX96: 0
         });
