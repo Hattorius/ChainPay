5df160d3d56040c039c29437c1b0e8adefe6003e
Aaron Jonk <AJ@jonk.name>
2024-08-20 19:16:18 +0200

    Switch to uniswap v3 protocol

--- src/contracts/ChainPay.sol
+++ src/contracts/ChainPay.sol
@@ -4,30 +4,37 @@ pragma solidity ^0.8.0;
 // Interfaces
 import "./IERC20.sol";
 import "./Ownable.sol";
-import "./IUniswapV2Router02.sol";
+import "./ISwapRouter.sol";
 import "./IWrapped.sol";
+import "./TransferHelper.sol";
 
 
 contract ChainPay is Ownable {
-    IUniswapV2Router02 public immutable uniswapRouter;
+    ISwapRouter public immutable swapRouter;
     IWrapped public immutable wrappedCoin;
-    address public constant UNISWAP_V2_ROUTER = 0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24; // BNB chain
+    address public constant SUSHISWAP_V3_ROUTER_ADDRESS = 0x909662a99605382dB1E8d69cc1f182bb577d9038; // BNB chain
     address public constant WRAPPED_COIN = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c; // BNB chain, wrapped BNB
 
     constructor() {
-        uniswapRouter = IUniswapV2Router02(UNISWAP_V2_ROUTER);
+        swapRouter = ISwapRouter(SUSHISWAP_V3_ROUTER_ADDRESS);
         wrappedCoin = IWrapped(WRAPPED_COIN);
     }
 
-    function swap(address tokenIn, address tokenOut, uint256 amountIn, uint256 amountOut) internal {
-        uint256 tokenOutBalance = IERC20(tokenOut).balanceOf(address(this));
-        require(tokenOutBalance >= amountIn, "Insufficient token balance");
+    function swap(address tokenIn, address tokenOut, uint256 amountIn, uint256 amountOutMax, uint24 fee) internal {
+        TransferHelper.safeApprove(IERC20(tokenIn), address(swapRouter), tokenIn);
+        
+        IERC20(tokenIn).approve(address(swapRouter), amountIn);
 
-        IERC20(tokenOut).approve(address(UNISWAP_V2_ROUTER), tokenOutBalance);
-
-        address[] memory path;
-        path[0] = tokenIn;
-        path[1] = tokenOut;
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams({
+            tokenIn: tokenIn,
+            tokenOut: tokenOut,
+            fee: fee, // 3000 = 0.3%
+            recipient: address(this),
+            deadline: block.timestamp + 15,
+            amountIn: amountIn,
+            amountOutMinimum: amountOutMinimum,
+            sqrtPriceLimitX96: 0
+        });
 
         uniswapRouter.swapExactTokensForTokens(
             tokenOutBalance,
@@ -38,5 +45,5 @@ contract ChainPay is Ownable {
         );
     }
 
-    
+
 }
