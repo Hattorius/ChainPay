ae55527acf2fd4c85b55d4c2972a550f7852728b
Aaron Jonk <AJ@jonk.name>
2024-08-22 10:46:55 +0200

    Apparently I just ran prettier write for the first time on the site. My fault

--- site/src/lib/TokenPicker.svelte
+++ site/src/lib/TokenPicker.svelte
@@ -2,22 +2,28 @@
 	import tokens from '$lib/tokens.json';
 	import { getAddress } from 'viem';
 	import ImageLazyLoad from './ImageLazyLoad.svelte';
-	import { onMount, tick } from 'svelte';
+	import { createEventDispatcher, onMount, tick } from 'svelte';
 
-    let wrapper: HTMLDivElement;
+	let wrapper: HTMLDivElement;
 	let tokensList: typeof tokens = [];
 	let selected: (typeof tokens)[0] | null = null;
 	let container: HTMLDivElement;
 	let isOpen = false;
 	let tokenInput: HTMLInputElement;
-    let tokenInputValue: string;
+	let tokenInputValue: string;
+	const dispatch = createEventDispatcher();
 
 	$: {
-        if (isOpen && tokenInputValue &&tokenInputValue != '') {
-            tokensList = tokens.filter(t => t.name.includes(tokenInputValue) || t.symbol.includes(tokenInputValue) || t.id.includes(tokenInputValue));
-        } else {
-            tokensList = tokens;
-        }
+		if (isOpen && tokenInputValue && tokenInputValue != '') {
+			tokensList = tokens.filter(
+				(t) =>
+					t.name.includes(tokenInputValue) ||
+					t.symbol.includes(tokenInputValue) ||
+					t.id.includes(tokenInputValue)
+			);
+		} else {
+			tokensList = tokens;
+		}
 	}
 
 	const open = async () => {
@@ -26,45 +32,54 @@
 		tokenInput.focus();
 	};
 
-    const close = () => {
-        isOpen = false;
-    }
+	const close = () => {
+		isOpen = false;
+	};
 
-    const select = (token: (typeof tokens)[0]) => {
-        selected = token;
-        close();
-    }
+	const select = (token: (typeof tokens)[0]) => {
+		dispatch('change', token);
+		selected = token;
+		close();
+	};
 
-    const clickHandler = (e: MouseEvent) => {
-        const target = e.target as Element;
-        if (target && !wrapper.contains(target)) {
-            close();
-        }
-    }
+	const clickHandler = (e: MouseEvent) => {
+		const target = e.target as Element;
+		if (target && !wrapper.contains(target)) {
+			close();
+		}
+	};
 
-    onMount(() => {
-        document.addEventListener('mousedown', clickHandler);
-        return () => document.removeEventListener('mousedown', clickHandler);
-    });
+	onMount(() => {
+		document.addEventListener('mousedown', clickHandler);
+		return () => document.removeEventListener('mousedown', clickHandler);
+	});
 </script>
 
 <div class="border cursor-pointer relative" bind:this={wrapper}>
 	{#if isOpen}
-		<input type="text" class="border-none bg-black w-full text-base p-2" bind:this={tokenInput} bind:value={tokenInputValue} />
+		<input
+			type="text"
+			class="border-none bg-black w-full text-base p-2 focus:outline-none focus:border-none focus:rounded-none"
+			bind:this={tokenInput}
+			bind:value={tokenInputValue}
+		/>
 	{:else if !selected}
 		<!-- svelte-ignore a11y-click-events-have-key-events -->
 		<div class="p-2" on:click={open}>Press here</div>
-    {:else if selected}
-        <!-- svelte-ignore a11y-click-events-have-key-events -->
-        <div class="p-2 flex gap-2" on:click={open}>
-            <div class="h-6 w-6">
-                <img src={`https://tokens.pancakeswap.finance/images/${getAddress(selected.id)}.png`} alt={selected.name}/>
-            </div>
-            
-            <p class="flex-1">
-                {selected.name} ({selected.symbol})
-            </p>
-        </div>
+	{:else if selected}
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<div class="p-2 flex gap-2" on:click={open}>
+			<div class="h-6 w-6">
+				<img
+					src={`https://tokens.pancakeswap.finance/images/${getAddress(selected.id)}.png`}
+					alt={selected.name}
+				/>
+			</div>
+
+			<p class="flex-1">
+				{selected.name} ({selected.symbol})
+			</p>
+		</div>
 	{/if}
 
 	{#if isOpen}
@@ -74,7 +89,10 @@
 		>
 			{#each tokensList as token}
 				<!-- svelte-ignore a11y-click-events-have-key-events -->
-				<div class="flex items-center gap-2 py-1 px-2 hover:bg-gray-900 transition" on:click={() => select(token)}>
+				<div
+					class="flex items-center gap-2 py-1 px-2 hover:bg-gray-900 transition"
+					on:click={() => select(token)}
+				>
 					<div class="w-8 h-8">
 						<ImageLazyLoad
 							src={`https://tokens.pancakeswap.finance/images/${getAddress(token.id)}.png`}
