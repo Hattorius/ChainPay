c1f5c2459439247352be4a6e90062e20158d7949
Aaron Jonk <AJ@jonk.name>
2024-08-22 12:55:19 +0200

    Apparently was pnpm cache, not my types

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,7 +1,10 @@
+import { CreateTransactionInput, TransactionType } from './types';
 import createTransaction from './createTransaction';
 import utils from './utils';
 
+export { CreateTransactionInput, TransactionType, createTransaction };
+
 export default {
-	...utils,
+	utils,
 	createTransaction
 };
