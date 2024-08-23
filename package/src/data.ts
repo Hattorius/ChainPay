d71706001636a7e85d8e646708b4e0627399b6ec
Aaron Jonk <AJ@jonk.name>
2024-08-23 16:17:58 +0200

    small clean up

--- package/src/data.ts
+++
@@ -1,6 +0,0 @@
-const pre = '$!$!$ CHAINPAY $!$!$\n';
-
-const datafy = (data: any) => `${pre}${JSON.stringify(data)}`;
-const undatafy = (data: string) => JSON.parse(data.substring(pre.length));
-
-export { datafy, undatafy };
