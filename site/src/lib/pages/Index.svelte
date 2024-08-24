cf34d5858f3bd158b7f8a35be75bec6c595ff9d9
Aaron Jonk <AJ@jonk.name>
2024-08-24 02:51:43 +0200

    Different diagram

--- site/src/lib/pages/Index.svelte
+++ site/src/lib/pages/Index.svelte
@@ -10,8 +10,8 @@
 	</div>
 
 	<p>
-		ChainPay is an open-source payment solution deployed on Binance Smart Chain (BSC) that allows
-		users and contracts to receive payments in any token of their choice. It automatically swaps the
+		ChainPay is an open-source payment solution deployed on Binance Smart Chain that allows users
+		and contracts to receive payments in any token of their choice. It automatically swaps the
 		sender's token to the recipient's preferred token, simplifying multi-token payments.
 	</p>
 </div>
