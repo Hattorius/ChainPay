7b954c5ebd3c856cb595683d494d792ffc55fb55
Aaron Jonk <AJ@jonk.name>
2024-08-24 13:06:30 +0200

    docs?

---
+++ docs/docs/JS-SDK/install.md
@@ -0,0 +1,7 @@
+---
+sidebar_position: 1
+---
+
+# Install the SDK
+
+Hello
---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,71 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'ChainPay - docs',
+  tagline: 'On chain payments on the BNB chain',
+  url: 'https://your-docusaurus-site.example.com',
+  baseUrl: '/',
+  organizationName: 'Hattorius',
+  projectName: 'ChainPay',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: true
+    },
+    navbar: {
+      title: 'ChainPay',
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'ideaSidebar',
+          position: 'left',
+          label: 'Docs',
+        },
+        {
+          href: 'https://github.com/Hattorius/ChainPay',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} ChainPay, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig
+};
+
+export default config;
