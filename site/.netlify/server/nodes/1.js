be28a6b7ab665f2e927f4d07500f61a274ca294a
Aaron Jonk <AJ@jonk.name>
2024-08-24 12:12:46 +0200

    Remove netlify files

--- site/.netlify/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.d7019193.js","_app/immutable/chunks/index.00646974.js","_app/immutable/chunks/singletons.084dae75.js","_app/immutable/chunks/index.b1edd31c.js"];
-export const stylesheets = [];
-export const fonts = [];
--- site/.netlify/server/nodes/3.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 3;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/docs/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/3.c9513b46.js","_app/immutable/chunks/index.00646974.js"];
-export const stylesheets = [];
-export const fonts = [];
