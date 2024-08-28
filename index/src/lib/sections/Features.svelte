445d16be7316d3a4dc6622c160c00b5985fde018
Aaron Jonk <AJ@jonk.name>
2024-08-28 20:06:57 +0200

    Key features section

---
+++ index/src/lib/sections/Features.svelte
@@ -0,0 +1,47 @@
+<div class="features">
+	<div class="wrapper">
+		<h2>Key Features</h2>
+	</div>
+
+	<div>
+		<slot />
+	</div>
+</div>
+
+<style lang="scss">
+	div.features {
+		background: #1d263b;
+		padding: 70px 0;
+		width: 100%;
+
+		div.wrapper {
+			max-width: 1400px;
+			width: 100%;
+			margin: 0 auto;
+		}
+
+		& > div:not(.wrapper) {
+			max-width: 1400px;
+			width: 100%;
+			margin: 40px auto 0 auto;
+			border-radius: 20px;
+			border: 2px solid rgba(170, 114, 206, 0.12);
+			height: 170px;
+
+			display: flex;
+			justify-content: space-evenly;
+
+			:global(div:nth-child(odd)) {
+				width: 250px;
+				text-align: center;
+				padding: 20px 0;
+			}
+
+			:global(div:nth-child(even)) {
+				background: rgba(170, 114, 206, 0.12);
+				width: 2px;
+				height: 100%;
+			}
+		}
+	}
+</style>
--- index/src/routes/+page.svelte
+++ index/src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import Features from '$lib/sections/Features.svelte';
 	import Hero from '$lib/sections/Hero.svelte';
 	import Info from '$lib/sections/Info.svelte';
 	import { onMount } from 'svelte';
@@ -13,12 +14,51 @@
 <Hero />
 
 <Info>
-	<h1>What is it?</h1>
+	<h2>What is it?</h2>
 
 	<p class="info-text">
-		ChainPay is an open-source payment solution deployed on Binance Smart Chain that allows users and contracts to receive payments in any token of their choice. It automatically swaps the sender's token to the recipient's preferred token, simplifying multi-token payments.
+		ChainPay is an open-source payment solution deployed on Binance Smart Chain that allows users
+		and contracts to receive payments in any token of their choice. It automatically swaps the
+		sender's token to the recipient's preferred token, simplifying multi-token payments.
 	</p>
 </Info>
+
+<Features>
+	<div>
+		<h3>Token Flexibility</h3>
+
+		<p>
+			Send and receive payments in any supported token, regardless of what token the sender holds.
+		</p>
+	</div>
+
+	<div />
+
+	<div>
+		<h3>Automatic Swaps</h3>
+
+		<p>Integrated with PancakeSwap for seamless swaps between tokens during payments.</p>
+	</div>
+
+	<div />
+
+	<div>
+		<h3>Developer Friendly</h3>
+
+		<p>Open-source, easy-to-integrate with robust developer documentation.</p>
+	</div>
+
+	<div />
+
+	<div>
+		<h3>Token Flexibility</h3>
+
+		<p>
+			Currently on BSC, but designed to expand to multiple EVM chains and support chain bridges in
+			future releases.
+		</p>
+	</div>
+</Features>
 
 <style>
 	.info-text {
